2dbaa1f98680f989281797ef9ab505f4792685aa
Jobson Almeida <jobsonbox@gmail.com>
2025-02-06 14:58:38 -0300

    Function play_note is working

--- SongExpBox/SongExpBox.c
+++ SongExpBox/SongExpBox.c
@@ -1,85 +1,122 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
+#include "hardware/clocks.h"
 
-#define BUZZER_PIN 21  // GPIO onde o buzzer está conectado
-#define LED_GREEN 11
-#define LED_RED 13
+#define BUZZER_LEFT_1 21  
+#define BUZZER_RIGHT_1 10
+
+#define A0_FREQUENCY 27.5
+#define A0_WRAP 65530
+
+uint32_t pwm_clock_hz;
+
+typedef struct {
+    uint16_t wrap;
+    float divisor;
+} Adjustment;
 
 
-uint slice_num = 0;
+Adjustment get_divisor_wrap_from_note(char note[3]){
 
+    static Adjustment adjustment; 
 
-void set_buzzer_frequency(uint buzzer_gpio, uint freq) {
-    slice_num = pwm_gpio_to_slice_num(buzzer_gpio);
-    
-    // Calcular o clock divisor e a contagem para definir a frequência
-    uint32_t clock_div = 4;  // Ajuste conforme necessário
-    uint32_t wrap = (125000000 / (clock_div * freq)) - 1;
+    if(note[2] == '0'){
 
-    pwm_set_clkdiv(slice_num, clock_div);
-    pwm_set_wrap(slice_num, wrap);
-    pwm_set_gpio_level(buzzer_gpio, wrap / 2);  // 50% duty cycle
-    pwm_set_enabled(slice_num, true);
+        if(note[0] == 'C'){
+                 if(note[1] == '-') {    }
+            else if(note[1] == '#') {}
+            else                    {  }
+        }
+
+        else if(note[0] == 'D'){
+            if(note[1] == '-') {}
+            else if(note[1] == '#') {}
+            else {}
+        }
+        else if(note[0] == 'E'){
+            if(note[1] == '-') {}
+            else if(note[1] == '#') {}
+            else {}
+        }
+        else if(note[0] == 'F'){
+            if(note[1] == '-') {}
+            else if(note[1] == '#') {}
+            else {}
+        }
+        else if(note[0] == 'G'){
+            if(note[1] == '-') {}
+            else if(note[1] == '#') {}
+            else {}
+        }
+        else if(note[0] == 'A'){
+                 if(note[1] == '-') { adjustment.wrap = A0_WRAP; adjustment.divisor = pwm_clock_hz/(A0_FREQUENCY * (adjustment.wrap+1)); printf("encontrou nota A-0\n"); }
+            else if(note[1] == '#') {}
+            else {}
+        }
+        else if(note[0] == 'B'){
+            if(note[1] == '-') {}
+            else if(note[1] == '#') {}
+            else {}
+        }
+
+    }    
+
+    printf("adjustment.divisor = %.3f - adjustment.wrap = %d \n", adjustment.divisor , adjustment.wrap);
+ 
+    return adjustment;
+
+}
+
+void play_note(uint buzzer_id, char note[3], float duty_cycle, float time_ms){
+
+    printf("Dentro de playnote\n");
+
+    // static varibale so the they are not created every time
+    static uint slice = 2;
+    static uint channel = 1;
+    static Adjustment adjustement = {0, 0.0f};
+
+    slice = pwm_gpio_to_slice_num(buzzer_id);
+    channel = pwm_gpio_to_channel(buzzer_id);    
+
+    adjustement = get_divisor_wrap_from_note(note);
+    pwm_set_clkdiv(slice, adjustement.divisor);
+    pwm_set_wrap(slice,  adjustement.wrap);
+
+    pwm_set_chan_level(slice, channel, duty_cycle*(adjustement.wrap+1));
+
+    pwm_set_enabled(slice, true);
+    sleep_ms(time_ms);
+    pwm_set_enabled(slice, false);
+
 }
 
 int main() {
+
     stdio_init_all();
+    sleep_ms(15000);
+    printf("Programa executando\n");
 
-    gpio_set_function(BUZZER_PIN, GPIO_FUNC_SIO);
-    gpio_set_dir(BUZZER_PIN, GPIO_OUT); 
+    pwm_clock_hz = clock_get_hz(clk_sys);
+    printf("Frequencia fora while: %u Hz\n", pwm_clock_hz);
 
-    gpio_set_function(LED_GREEN, GPIO_FUNC_SIO);
-    gpio_set_dir(LED_GREEN, GPIO_OUT); 
+    //Initial Configurations
+    gpio_set_function(BUZZER_LEFT_1, GPIO_FUNC_PWM);
+    gpio_set_function(BUZZER_RIGHT_1, GPIO_FUNC_PWM);
 
-    gpio_set_function(LED_RED, GPIO_FUNC_SIO);
-    gpio_set_dir(LED_RED, GPIO_OUT); 
+    sleep_ms(4000);
 
-    while (1) {
-        // set_buzzer_frequency(BUZZER_PIN, 1000); // Toca 1kHz
-        // sleep_ms(1000);
-        // set_buzzer_frequency(BUZZER_PIN, 1500); // Toca 1.5kHz
-        // sleep_ms(1000);
-        // pwm_set_enabled(slice_num, false);
-        // sleep_ms(2000);
+    printf("Antes de playnote \n");
 
-        
-        gpio_put(BUZZER_PIN, 1); 
-        gpio_put(LED_GREEN, 1); 
-        gpio_put(LED_RED, 0); 
-        sleep_ms(1000);
-        gpio_put(BUZZER_PIN, 0); 
-        gpio_put(LED_GREEN, 0); 
-        gpio_put(LED_RED, 1); 
-        sleep_ms(1000);
+    play_note(BUZZER_RIGHT_1, "A-0", 0.5, 10000);
 
-        gpio_put(BUZZER_PIN, 1); 
-        gpio_put(LED_GREEN, 1); 
-        gpio_put(LED_RED, 0); 
-        sleep_ms(1000);
-        gpio_put(BUZZER_PIN, 0); 
-        gpio_put(LED_GREEN, 0); 
-        gpio_put(LED_RED, 1); 
-        sleep_ms(1000);
-
-        gpio_put(BUZZER_PIN, 1); 
-        gpio_put(LED_GREEN, 1); 
-        gpio_put(LED_RED, 0); 
-        sleep_ms(1000);
-        gpio_put(BUZZER_PIN, 0); 
-        gpio_put(LED_GREEN, 0); 
-        gpio_put(LED_RED, 1); 
-        sleep_ms(1000);
-
-        gpio_put(BUZZER_PIN, 1); 
-        gpio_put(LED_GREEN, 1); 
-        gpio_put(LED_RED, 0); 
-        sleep_ms(1000);
-        gpio_put(BUZZER_PIN, 0); 
-        gpio_put(LED_GREEN, 0); 
-        gpio_put(LED_RED, 1); 
-        sleep_ms(1000);
-        
-
+    // O loop vazio. 
+    while (true) {
+         printf("A frequência do clock do PWM (clk_sys) é: %u Hz\n", pwm_clock_hz);
+         sleep_ms(1000);
     }
+
+    return 0;
+   
 }
