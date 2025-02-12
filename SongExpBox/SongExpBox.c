9053db38c339e6e459da1afe779649486a259071
Jobson Almeida <jobsonbox@gmail.com>
2025-02-11 21:54:54 -0300

    Display working

--- SongExpBox/SongExpBox.c
+++ SongExpBox/SongExpBox.c
@@ -1,141 +1,121 @@
+ // ----------- ADC e DMA -------------
+
+
+// #include <stdio.h>
+// #include "pico/stdlib.h"
+// #include "hardware/adc.h"
+// #include "hardware/dma.h"
+
+// #include "peripherals/ADC_DMA/ADC_DMA_functions.h"
+
+// extern uint16_t x_axis_buffer[];
+// extern uint16_t y_axis_buffer[];
+
+// extern int dma_chan_0;
+// extern int dma_chan_1;
+
+// int main() {
+
+//     stdio_init_all();
+//     setup_adc_and_dma(); // Inicia o adc e o dma correpondente ao adc  
+
+//     while (true) {
+
+//         joystick_data joystick_data = velocity_and_direction(x_axis_buffer, y_axis_buffer);
+
+//         printf("joystick_data.velocity_x %d - joystick_data.velocit_y: %d\n", joystick_data.velocity_x, joystick_data.velocity_y);        
+
+//         sleep_ms(10);                  
+          
+//     };
+        
+//     sleep_ms(10);    
+
+//     return 0;
+ 
+// }
+
+// ----------- ADC e DMA -------------
+
+
+
+
+// --------------- DISPLAY ----------------------------
+
+
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/adc.h"
-#include "hardware/dma.h"
+#include "hardware/pwm.h"
+#include "hardware/clocks.h"
+#include "hardware/i2c.h"
 
-#include "peripherals/ADC_DMA/ADC_DMA_functions.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "peripherals/I2C_Display/ssd1306.h"
+#include "hardware/i2c.h"
 
-extern uint16_t x_axis_buffer[];
-extern uint16_t y_axis_buffer[];
 
-extern int dma_chan_0;
-extern int dma_chan_1;
+const uint I2C_SDA = 14;
+const uint I2C_SCL = 15;
 
-int main() {
+int main()
+{
+    stdio_init_all();   // Inicializa os tipos stdio padrão presentes ligados ao binário
 
-    stdio_init_all();
-    setup_adc_and_dma(); // Inicia o adc e o dma correpondente ao adc  
+    // Inicialização do i2c
+    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
 
-    while (true) {
+    // Processo de inicialização completo do OLED SSD1306
+    ssd1306_init();
 
-        joystick_data joystick_data = velocity_and_direction(x_axis_buffer, y_axis_buffer);
-
-        printf("joystick_data.velocity_x %d - joystick_data.velocit_y: %d\n", joystick_data.velocity_x, joystick_data.velocity_y);        
-
-        sleep_ms(10);                  
-          
+    // Preparar área de renderização para o display (ssd1306_width pixels por ssd1306_n_pages páginas)
+    struct render_area frame_area = {
+        start_column : 0,
+        end_column : ssd1306_width - 1,
+        start_page : 0,
+        end_page : ssd1306_n_pages - 1
     };
 
-        // const uint bar_width = 10;
-        // const uint adc_max = (1 << 12) - 1;
-        // uint bar_x_pos = buffer_adc_x[0] * bar_width / adc_max;
-        
-        // if (adc_fifo_is_empty() == 0) { // Verifica se há dado no FIFO
-        //     uint16_t adc_value = adc_fifo_get(); // Lê o valor convertido
-        //     printf("ADC Value: %d\n", adc_value);
-        // }
+    calculate_render_area_buffer_length(&frame_area);
 
-        // printf("buffer_adc_x[0]: %d\n", buffer_adc_x[0]);
+    // zera o display inteiro
+    uint8_t ssd[ssd1306_buffer_length];
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
 
-        // if (dma_channel_is_busy(dma_chan)) {
-        //     printf("DMA está transferindo dados...\n");
-        // } else {
-        //     printf("DMA não está ativo! Reiniciando DMA...\n");
-        //     dma_channel_start(dma_chan);
-        // }
-        
-        sleep_ms(10);
-    
+// Parte do código para exibir a mensagem no display (opcional: mudar ssd1306_height para 32 em ssd1306_i2c.h)
+// /**
+    char *text[] = {
+        "Bem-vindos!   ",
+        "Embarcatech   "};
+
+    int y = 0;
+    for (uint i = 0; i < count_of(text); i++)
+    {
+        ssd1306_draw_string(ssd, 0, y, text[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
+// */
+
+
+    while(true) {
+        sleep_ms(1000);
+    }
 
     return 0;
- 
 }
 
 
-
-
-
-// // --------------- DISPLAY ----------------------------
-
-
-// #include <stdio.h>
-// #include "pico/stdlib.h"
-// #include "hardware/pwm.h"
-// #include "hardware/clocks.h"
-// #include "inc/ssd1306.h"
-// #include "hardware/i2c.h"
-
-// #include <stdio.h>
-// #include <string.h>
-// #include <stdlib.h>
-// #include <ctype.h>
-// #include "pico/stdlib.h"
-// #include "pico/binary_info.h"
-// #include "inc/ssd1306.h"
-// #include "hardware/i2c.h"
-
-// const uint I2C_SDA = 14;
-// const uint I2C_SCL = 15;
-
-
-
-
-// int main()
-// {
-//     stdio_init_all();   // Inicializa os tipos stdio padrão presentes ligados ao binário
-
-//     // Inicialização do i2c
-//     i2c_init(i2c1, ssd1306_i2c_clock * 1000);
-//     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-//     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-//     gpio_pull_up(I2C_SDA);
-//     gpio_pull_up(I2C_SCL);
-
-//     // Processo de inicialização completo do OLED SSD1306
-//     ssd1306_init();
-
-//     // Preparar área de renderização para o display (ssd1306_width pixels por ssd1306_n_pages páginas)
-//     struct render_area frame_area = {
-//         start_column : 0,
-//         end_column : ssd1306_width - 1,
-//         start_page : 0,
-//         end_page : ssd1306_n_pages - 1
-//     };
-
-//     calculate_render_area_buffer_length(&frame_area);
-
-//     // zera o display inteiro
-//     uint8_t ssd[ssd1306_buffer_length];
-//     memset(ssd, 0, ssd1306_buffer_length);
-//     render_on_display(ssd, &frame_area);
-
-// restart:
-
-// // Parte do código para exibir a mensagem no display (opcional: mudar ssd1306_height para 32 em ssd1306_i2c.h)
-// // /**
-//     char *text[] = {
-//         "  Bem-vindos!   ",
-//         "  Embarcatech   "};
-
-//     int y = 0;
-//     for (uint i = 0; i < count_of(text); i++)
-//     {
-//         ssd1306_draw_string(ssd, 5, y, text[i]);
-//         y += 8;
-//     }
-//     render_on_display(ssd, &frame_area);
-// // */
-
-
-//     while(true) {
-//         sleep_ms(1000);
-//     }
-
-//     return 0;
-// }
-
-
-// // __________________ END DISPLAY ________________
+// __________________ END DISPLAY ________________
 
 // // #include "./constants/GPIO_Constants.h"
 // // #include "./constants/Musical_Note_Constants.h"
