a5a5773325cf69a05b4db6e0789905db2cebcfa2
Jobson Almeida <jobsonbox@gmail.com>
2025-02-18 16:05:37 -0300

    Deleted junk files and folders

--- SongExpBox/constants/GPIO_Constants.h
+++
@@ -1,5 +0,0 @@
-
-#pragma once
-
-#define BUZZER_LEFT_1 21  
-#define BUZZER_RIGHT_1 10
--- SongExpBox/headers/play_note.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void play_note(uint buzzer_id, char note[3], float duty_cycle, float time_ms);
